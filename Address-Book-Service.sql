d33da5ab8361fbf1e27f272839980ce089e21064
unknown <narmada.vaddireddy97@gmail.com>
2023-03-31 18:38:53 +0530

    Added: Code to create a address book table

--- Address-Book-Service.sql
+++ Address-Book-Service.sql
@@ -1,3 +1,15 @@
 #UC1
 create database address_book_service;
 use address_book_service;
+#UC2
+create table address_book_service(
+FirstName varchar(20) not null ,
+LastName varchar(20) not null ,
+Address varchar(100) not null ,
+City varchar(30) not null ,
+State varchar(30) not null ,
+Email varchar(30) not null ,
+ZipCode int not null ,
+Phone_number double not null
+);
+describe address_book_service;
